4b299a043f5fd07673f16e0a5e1848768ef41333
Zac Campbell <zac2013@gmail.com>
2014-10-26 21:25:26 -0700

    push

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
+  get 'ads/index'
+
   get 'welcome/index'
 
-  root 'welcome#index'
+  root 'ads#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
