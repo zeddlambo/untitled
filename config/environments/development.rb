7b108a3733d2deae3a573368c3eb368a1b38b0b0
Zac Campbell <zac2013@gmail.com>
2014-10-26 19:30:16 -0700

    everything good

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Promotely.configure do
+promotely.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
