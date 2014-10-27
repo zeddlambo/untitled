668e58edb8e034e48dd2bae7b40bbc44b9782ba3
Zac Campbell <zac2013@gmail.com>
2014-10-26 21:19:22 -0700

    puish

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root :to => 'index#application.html.erb'
+  root :to => 'index#application'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
